6d4565b66473f3d2bd689440885aac719b1633a3
jjas0nn <jason.jen.2010@gmail.com>
2018-11-04 13:13:34 -0800

    jj

--- tohtml.sh
+++ tohtml.sh
@@ -2,7 +2,7 @@ jupyter nbconvert *.ipynb
 rm -rf html/
 mkdir html
 mv *.html html/
-scp -r html/* jasjen13@redfern.dreamhost.com:jj-jj.xyz/
+scp -r html/* jasjen13@redfern.dreamhost.com:jj-jj.org/
 git add --all
 git commit -m "jj"
 git push origin master
